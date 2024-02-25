3ddec1ede7fad0bbe2cbbc3ca918d08dcb971816
Mit Amin <mit.na14@gmail.com>
2024-02-24 20:07:59 -0500

    Initial Commit

---
+++ PhotoSwipe/PhotoSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwipeApp.swift
+//  PhotoSwipe
+//
+//  Created by Mit Amin on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
